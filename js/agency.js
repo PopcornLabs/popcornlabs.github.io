6ed3b4a6c00dbc9f8226996b0800aab70a492ef6
Sean Langley <slangley@popcornlabs.com>
2026-02-26 22:08:08 -0500

    Updates

--- js/agency.js
+++ js/agency.js
@@ -1,37 +1,39 @@
 /*!
- * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ * Agency Theme - Vanilla JS (no jQuery)
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+(function () {
+    "use strict";
+
+    // Navbar shrink on scroll
+    var navbar = document.getElementById("mainNav");
+    if (navbar) {
+        window.addEventListener("scroll", function () {
+            if (window.scrollY > 100) {
+                navbar.classList.add("navbar-shrink");
+            } else {
+                navbar.classList.remove("navbar-shrink");
+            }
+        });
+    }
+
+    // Smooth scrolling for page-scroll links
+    document.querySelectorAll("a.page-scroll").forEach(function (link) {
+        link.addEventListener("click", function (e) {
+            var href = this.getAttribute("href");
+            if (href && href.startsWith("#")) {
+                e.preventDefault();
+                var target = document.querySelector(href);
+                if (target) {
+                    target.scrollIntoView({ behavior: "smooth" });
+                }
+                // Collapse mobile nav if open
+                var navCollapse = document.querySelector(".navbar-collapse.show");
+                if (navCollapse) {
+                    var bsCollapse = bootstrap.Collapse.getInstance(navCollapse);
+                    if (bsCollapse) bsCollapse.hide();
+                }
+            }
+        });
     });
-});
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
-});
+})();
